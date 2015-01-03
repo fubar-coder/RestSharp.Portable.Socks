ab09ef855e64f06c74be9f958a283d002157cd92
Mark Junker <mark.junker75@gmail.com>
2015-01-03 12:25:59 +0100

    First test prototypes

---
+++ RestSharp.Portable.Socks.NET45.Tests/NetworkConverterTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestSharp.Portable.Socks.NET45.Tests
+{
+    public class NetworkConverterTests
+    {
+        [Theory]
+        [InlineData("2001:0db8:85a3:08d3:1319:8a2e:0370:7344", new ushort[] { 0x2001, 0x0db8, 0x85a3, 0x08d3, 0x1319, 0x8a2e, 0x0370, 0x7344 })]
+        [InlineData("2001:db8:0:8d3:0:8a2e:70:7344", new ushort[] { 0x2001, 0xdb8, 0x0, 0x8d3, 0x0, 0x8a2e, 0x70, 0x7344 })]
+        [InlineData("2001:db8::1428:57ab", new ushort[] { 0x2001, 0x0db8, 0x0, 0x0, 0x0, 0x0, 0x1428, 0x57ab })]
+        [InlineData("2001:db8:0:0:8d3::", new ushort[] { 0x2001, 0x0db8, 0x0, 0x0, 0x8d3, 0x0, 0x0, 0x0 })]
+        [InlineData("2001:db8::8d3:0:0:0", new ushort[] { 0x2001, 0x0db8, 0x0, 0x0, 0x8d3, 0x0, 0x0, 0x0 })]
+        [InlineData("::ffff:127.0.0.1", new ushort[] { 0x0, 0x0, 0x0, 0x0, 0x0, 0xffff, 0x7f00, 0x1 })]
+        [InlineData("::ffff:7f00:1", new ushort[] { 0x0, 0x0, 0x0, 0x0, 0x0, 0xffff, 0x7f00, 0x1 })]
+        public void TestParseIPv6(string ipv6Text, ushort[] ipv6Data)
+        {
+            var data = NetworkConverter.GetWordsForIPv6(ipv6Text);
+            Assert.Equal(ipv6Data, data);
+        }
+
+        [Theory]
+        [InlineData("::1", true)]
+        [InlineData("::ffff:127.0.0.1", true)]
+        [InlineData("::ffff:7f00:1", true)]
+        [InlineData("::ffff:127.0.0.2", false)]
+        [InlineData("::ffff:7f01:1", false)]
+        [InlineData("::ffff:7f00:2", false)]
+        [InlineData("127.0.0.1", true)]
+        [InlineData("127.0.0.2", false)]
+        [InlineData("127.0.1.1", false)]
+        [InlineData("127.1.0.1", false)]
+        public void TestIsLocalHost(string ipv6Text, bool isLoopBack)
+        {
+            Assert.Equal(isLoopBack, NetworkConverter.IsLoopBack(ipv6Text));
+        }
+    }
+}
