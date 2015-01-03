de0e51989265ac062a90a19ed46cf434b7cfcd32
Mark Junker <mark.junker75@gmail.com>
2015-01-03 16:05:57 +0100

    Tests for SOCKS5

---
+++ RestSharp.Portable.Socks.NET45.Tests/Socks5Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using RestSharp.Portable.Socks.Socks5;
+using Xunit;
+namespace RestSharp.Portable.Socks.NET45.Tests
+{
+    public class Socks5Tests
+    {
+        static RestClient CreateClientSocks5(bool useSsl, bool useHost)
+        {
+            bool ignoreSslErrors = !useHost && useSsl;
+            var client = new RestClient(string.Format("http{0}://httpbin.org/cookies", useSsl ? "s" : string.Empty))
+            {
+                HttpClientFactory = new Socks5HttpClientFactory(new TcpClientImpl.TcpClientWrapperFactory(ignoreSslErrors))
+                {
+                    ResolveHost = !useHost,
+                },
+                CookieContainer = new CookieContainer(),
+                Proxy = new Socks5WebProxy(new SocksAddress("localhost", 9150)),
+            };
+            return client;
+        }
+
+        [Theory]
+        [InlineData(false, false)]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        [InlineData(true, true)]
+        public async Task ExecuteHttpBinTests(bool useSsl, bool useHost)
+        {
+            var client = CreateClientSocks5(useSsl, useHost);
+            await TestSocksRequests.Test(client);
+        }
+    }
+}
--- RestSharp.Portable.Socks/Socks5/Socks5HttpClientFactory.cs
+++ RestSharp.Portable.Socks/Socks5/Socks5HttpClientFactory.cs
@@ -8,6 +8,8 @@ namespace RestSharp.Portable.Socks.Socks5
         private readonly ITcpClientFactory _tcpClientFactory;
         private readonly Pooling.TcpClientPool _pool;
 
+        public bool ResolveHost { get; set; }
+
         public Socks5HttpClientFactory(ITcpClientFactory tcpClientFactory)
         {
             _pool = new Pooling.TcpClientPool(tcpClientFactory);
@@ -21,7 +23,10 @@ namespace RestSharp.Portable.Socks.Socks5
             if (socksProxy == null)
                 return base.CreateMessageHandler(client, request);
 
-            var httpClientHandler = new HttpSocks5MessageHandler(_pool, socksProxy);
+            var httpClientHandler = new HttpSocks5MessageHandler(_pool, socksProxy)
+            {
+                ResolveHost = ResolveHost,
+            };
             var cookies = GetCookies(client, request);
             if (cookies != null)
             {
